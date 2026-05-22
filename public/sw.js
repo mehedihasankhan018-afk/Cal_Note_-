93a827d8ad3c6b8ddcb8f1e698a98f4e8b453ff4
mehedihasankhan018-afk <mehedihasankhan018@gmail.com>
2026-05-22 20:01:59 +0600

    fixed pwa install

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", () => {});
